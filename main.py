ef597c65ed6c7438adbbd79de5a0d8ac8a86e0d5
Jricardossilva <jeffersonricardoss10@gmail.com>
2024-08-20 16:13:47 -0300

    Criacao do ambiente virtual

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-#inteiro
+#Inteiro
+inteiro = 10
+
+#Flutuante
+flutuante = 10.0
+
+#Texto
+texto = 'Jefferson'
+print(texto[4:7])
+
+#booleano
+booleano = True
+
+#Lista
+lista = [10, 20, 30]
+print(lista[1])
+
+#Tupla
+tupla = (10, 20, 30)
+
+#Dicionario
+dicionario = {'chave': 'valor'}
+
+
+#Constante
+CONSTANT = 10
+
+#Desativar Policy
+"Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser"
